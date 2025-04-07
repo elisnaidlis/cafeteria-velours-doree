4f1005b02c2b69093f63dd32b834b62d0551b8cd
Elisnaidlis Correa <elisnaidlis@gmail.com>
2025-04-06 21:41:07 -0300

    Evolving Project

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ FormsModule, CommonModule ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+
+  // constructor (
+  //   public name: string,
+  //   public email: string,
+  //   public phone: number,
+  //   public subject: string,
+  //   public message: string
+  // ) {}
+
+  form: any = {
+    name: '',
+    email: ''
+  };
+
+  submitForm(formContact) {
+    console.log(formContact);
+  }
 
 }
