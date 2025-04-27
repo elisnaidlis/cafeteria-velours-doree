1a9dae95ffcbbbedcbeb0cdb1883a8d204b69b67
Elisnaidlis Correa <elisnaidlis@gmail.com>
2025-04-27 16:53:41 -0300

    improving contact page form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,8 @@ import { FooterComponent } from './shared/footer/footer.component';
       CommonModule,
       RouterOutlet,
       HeaderComponent,
-      FooterComponent
+      FooterComponent,
+      ContactComponent
     ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,31 +1,46 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [ FormsModule, CommonModule ],
+  imports: [ FormsModule, CommonModule, ReactiveFormsModule ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
 
-  // constructor (
-  //   public name: string,
-  //   public email: string,
-  //   public phone: number,
-  //   public subject: string,
-  //   public message: string
-  // ) {}
+  formContact: FormGroup;
 
-  form: any = {
-    name: '',
-    email: ''
-  };
-
-  submitForm(formContact) {
-    console.log(formContact);
+  constructor(private formBuilder: FormBuilder) {
+    this.formContact = this.formBuilder.group ({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.pattern(/^\(\d{2}\) \d{5}-\d{4}$/)]],
+      subject: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(30)]],
+      message: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(400)]]
+    })
   }
 
+  checkField(field: string): string {
+    const control = this.formContact.get(field);
+    return control && control.invalid && (control.dirty || control.touched) ? 'is-invalid' : '';
+  }
+
+  submitForm() {
+    if (this.formContact.valid) {
+      console.log('Formulário enviado:', this.formContact.value);
+    } else {
+      console.log('Formulário inválido');
+      this.formContact.markAllAsTouched();
+    }
+
+    this.formContact.reset();
+
+  }
 }
+
+
+//private http: HttpClient
